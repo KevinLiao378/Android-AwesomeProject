cc963a41497940d8226f3505f7cd23f502de1cb8
liaoyi <cooltut@hotmail.com>
2023-02-09 15:11:09 +0800

    fix: 开启 Hermes 引擎

--- lib-hybrid/src/main/java/com/kfang/base/hybrid/ReactNativeHostProvider.kt
+++ lib-hybrid/src/main/java/com/kfang/base/hybrid/ReactNativeHostProvider.kt
@@ -1,8 +1,11 @@
 package com.kfang.base.hybrid
 
 import android.app.Application
+import com.facebook.hermes.reactexecutor.HermesExecutor
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptExecutorFactory
 import com.facebook.react.shell.MainReactPackage
 
 /**
@@ -25,6 +28,13 @@ object ReactNativeHostProvider {
                 return listOf(
                     MainReactPackage()
                 )
+            }
+
+            // https://github.com/facebook/react-native/issues/34089#issuecomment-1299604266
+            // 解决 RN0.69.7 开启 Hermes 后 app 闪退问题。
+            override fun getJavaScriptExecutorFactory(): JavaScriptExecutorFactory {
+                HermesExecutor.loadLibrary()
+                return HermesExecutorFactory()
             }
         }
     }
